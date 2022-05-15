85835784b912306cd6136d7bbf682d27909e470b
sho-ts <rrv1011@icloud.com>
2022-05-15 18:44:57 +0900

    地球と箱くるくる

--- src/index.ts
+++ src/index.ts
@@ -16,34 +16,39 @@ const camera = new t.PerspectiveCamera(
 );
 camera.position.set(0, 0, +1000);
 
+const scene = new t.Scene();
+
+const render = () => renderer.render(scene, camera);
+
 const box = new t.Mesh(
   new t.BoxGeometry(100, 100, 100),
   new t.MeshStandardMaterial({
-    color: 0x5555ff,
+    color: 0x3366ff,
   })
 );
 
+box.position.x = 200;
+
 const sphere = new t.Mesh(
-  new t.SphereGeometry(300, 30, 30),
+  new t.SphereGeometry(100, 100, 100),
   new t.MeshStandardMaterial({
     map: new t.TextureLoader().load(earth),
   })
 );
 
 const light = new t.DirectionalLight(0xffffff);
-light.intensity = 1;
-light.position.set(1, 1, 1);
+light.position.set(1, 1, 90);
 
-const scene = new t.Scene();
-[light, sphere].forEach((item) => scene.add(item));
-
-const render = () => renderer.render(scene, camera);
+[light, sphere, box].forEach((item) => scene.add(item));
 
 const tick = () => {
   requestAnimationFrame(tick);
 
   sphere.rotation.x += 0.001;
   sphere.rotation.y += 0.001;
+
+  box.rotation.x += 0.01;
+  box.rotation.y += 0.01;
 
   render();
 };
