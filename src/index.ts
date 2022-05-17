c7cc4b6045e37cf1a0367761d3a9ae5435f0af22
sho-ts <rrv1011@icloud.com>
2022-05-17 22:58:44 +0900

    スクリーン座標を算出する

--- src/index.ts
+++ src/index.ts
@@ -24,22 +24,28 @@ const tick = () => {
   earth.rotation.y += 0.01;
 
   const worldPosition = earth.getWorldPosition(new t.Vector3());
+  const project = worldPosition.project(app.camera);
+
+  const screenX = (window.innerWidth / 2) * (+project.x + 1.0);
+  const screenY = (window.innerHeight / 2) * (-project.y + 1.0);
 
   if (direction === "right") {
-    earth.position.x += 5;
-    earth.position.y -= 5;
-    earth.position.z += 5;
+    earth.position.x += 0.5;
+    earth.position.y -= 0.5;
+    earth.position.z -= 0.5;
   }
   if (direction === "left") {
-    earth.position.x -= 5;
-    earth.position.y += 5;
-    earth.position.z -= 5;
+    earth.position.x -= 0.5;
+    earth.position.y += 0.5;
+    earth.position.z += 0.5;
   }
 
   if (earth.position.x === 300) direction = "left";
   if (earth.position.x === -300) direction = "right";
 
   labelElement.innerHTML = `x: ${worldPosition.x}<br />y: ${worldPosition.y}<br />z: ${worldPosition.z}`;
+  labelElement.style.left = `${screenX}px`;
+  labelElement.style.top = `${screenY}px`;
 
   app.render();
 };
